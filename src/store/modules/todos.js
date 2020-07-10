3b1d6c84fcd3eb5072025aec04af19fb612fb99a
KamaranL <kamaran.layne@gmail.com>
2020-07-10 16:33:55 -0400

    created store, todos component and todos module

---
+++ src/store/modules/todos.js
@@ -0,0 +1,31 @@
+import Axios from "axios";
+
+const state = {
+  todos: [],
+};
+
+const getters = {
+  allTodos: (state) => state.todos,
+};
+
+const actions = {
+  async fetchTodos({ commit }) {
+    const response = await Axios.get(
+      "http://jsonplaceholder.typicode.com/todos"
+    );
+
+    // console.log(response.data);
+    commit("setTodos", response.data);
+  },
+};
+
+const mutations = {
+  setTodos: (state, todos) => (state.todos = todos),
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
